2a61bcbc82ea0297746fa438b418c5919623d07b
Pedro Matsubara <pedro.matsubara.pereira@gmail.com>
2025-05-10 16:03:18 -0300

    feat: add async handling middleware to avoid repeating code on controllers

--- src/controllers/awards.controller.js
+++ src/controllers/awards.controller.js
@@ -1,10 +1,6 @@
 import { findMinAndMaxAwardIntervals } from "../services/awards.service.js";
 
-export async function findMinAndMaxIntervals(req, res, next) {
-  try {
-    const result = findMinAndMaxAwardIntervals();
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
+export async function findMinAndMaxIntervals(req, res) {
+  const result = findMinAndMaxAwardIntervals();
+  res.json(result);
 }
--- src/routes/awards.routes.js
+++ src/routes/awards.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { findMinAndMaxIntervals } from "../controllers/awards.controller.js";
+import { asyncHandler } from "../middlewares/asyncHandler.js";
 
 const router = express.Router();
 
-router.get("/intervals", findMinAndMaxIntervals);
+router.get("/intervals", asyncHandler(findMinAndMaxIntervals));
 
 export default router;
